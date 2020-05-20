e4c8aa48e98cda245f46775f5c3510cbf9c966d5
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-21 00:33:24 +0530

    implemented client side authentication

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_P
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization");
+  if(req.method === "OPTIONS"){
+    return res.sendStatus(200);
+  }
+  next();
+})
+
 app.use(isAuth);
 
 app.use(
