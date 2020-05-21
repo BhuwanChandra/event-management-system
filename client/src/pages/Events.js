6d1383039872becf3676965f51f45a2be2c80a29
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-22 01:38:30 +0530

    added context and validation

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import Backdrop from "../components/Backdrop/Backdrop";
+import Modal from "../components/Modal/Modal";
+import "./Events.css";
 
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
-    return <h1>Events Page!</h1>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal 
+          title="Add Event" 
+          canConfirm={true} 
+          canCancel={true}
+          onCancel={this.modalCancelHandler}
+          onConfirm={this.modalConfirmHandler}
+          >
+            Modal Content
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
