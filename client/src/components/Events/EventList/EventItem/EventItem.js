531790ac4433b6a945c3487c7715057da4481e2f
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-25 00:33:58 +0530

    completed events ui

--- client/src/components/Events/EventList/EventItem/EventItem.js
+++ client/src/components/Events/EventList/EventItem/EventItem.js
@@ -6,11 +6,11 @@ const EventItem = props => {
     <li key={props.eventId} className="events__list-item">
       <div>
         <h1>{props.title}</h1>
-        <h2>$13.33</h2>
+        <h2>{`$${props.price} - ${new Date(props.date).toLocaleDateString()}`}</h2>
       </div>
       <div>
         {!(props.userId === props.creatorId) ? (
-          <button className="btn">View Details</button>
+          <button className="btn" onClick={props.onDetail.bind(this, props.eventId)}>View Details</button>
         ) : (
           <p>Your the owner of this event</p>
         )}
